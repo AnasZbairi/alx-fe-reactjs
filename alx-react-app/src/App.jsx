c41f0ca83bef4697cb762bc9784a8716add35efa
AnasZbairi <ana96box2@gmail.com>
2025-02-16 21:46:55 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,17 @@
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
 import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
