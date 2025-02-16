fbf64602da4afaa2f100944a3eff3407dd4b385f
AnasZbairi <ana96box2@gmail.com>
2025-02-16 22:02:26 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,12 @@
+import UserProfile from './components/UserProfile';
+function App() {
+    return (
+        <div>
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        </div>
+    );
+}
+export default App;
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
