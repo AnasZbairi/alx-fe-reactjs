c2c0849ac3cfd7dd62dd54c2301dc16d9029917f
AnasZbairi <ana96box2@gmail.com>
2025-02-22 18:25:15 +0100

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,3 +1,18 @@
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext'; // Import the UserContext
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
 import UserProfile from './components/UserProfile';
 function App() {
     return (
