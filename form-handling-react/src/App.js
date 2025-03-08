c43b1f5fec98ad3f18f702b3e8f8de381d068c9a
AnasZbairi <ana96box2@gmail.com>
2025-03-08 14:48:12 +0100

    Update App.js

--- form-handling-react/src/App.js
+++ form-handling-react/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
 
 function App() {
   return (
     <div>
       <h1>User Registration</h1>
+      <h2>Controlled Form</h2>
       <RegistrationForm />
+      <h2>Formik Form</h2>
+      <FormikForm />
     </div>
   );
 }
