2b7bae409751d577fbbe4ce7d8149cf18485d6a2
AnasZbairi <ana96box2@gmail.com>
2025-03-08 15:55:11 +0100

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem('isAuthenticated'); // Simulate authentication
+  const { isAuthenticated } = useAuth(); // Use the useAuth hook
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
